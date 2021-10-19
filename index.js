af961342df54a84868f79124e338a8f20890d820
phiducchinh <ducchinh2652000@gmail.com>
2021-10-19 21:46:22 +0700

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+//npm init
+//npm i express
+//npm i nodemon --save-dev
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
