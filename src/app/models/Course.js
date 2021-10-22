0b028c25e542bd965ca7e18b3b538a590b6878fd
phiducchinh <ducchinh2652000@gmail.com>
2021-10-22 13:57:11 +0700

    last

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -4,7 +4,7 @@ const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema(
+const CourseSchema = new Schema(
     {
         name: { type: String, required: true },
         description: { type: String },
@@ -18,13 +18,23 @@ const Course = new Schema(
     },
 );
 
-// Add plugins
+CourseSchema.query.sortable= function(req){
+    if(req.query.hasOwnProperty('_sort')){
+        const isValidtype=['asc','desc'].includes(req.query.type);
 
+        return this.sort({
+            [req.query.column]: isValidtype===true ? req.query.type :'desc',
+        });
+    }
+    return this;
+}
+
+// Add plugins
 mongoose.plugin(slug);
 
-Course.plugin(mongooseDelete, {
+CourseSchema.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
 });
 
-module.exports = mongoose.model('Course', Course);
+module.exports = mongoose.model('Course', CourseSchema);
--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ const methodOverride= require('method-override')
 const port = 3000
 const db = require('./config/db');
 
+const SortMiddleware= require('./app/middlewares/SortMiddleware.js')
+
 db.connect();
 
 app.use(express.static(path.join(__dirname,'public')));
@@ -29,17 +31,16 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({
   extended:true
 }))
+app.use(morgan('combined'))
 app.use(express.json())
 
 app.use(methodOverride('_method'));
 
-app.use(morgan('combined'))
+app.use(SortMiddleware);
 
 app.engine('hbs', handlebars({
   extname:'.hbs',
-  helpers:{
-    sum: (a,b)=> a+b,
-  }
+  helpers: require('./helpers/handlebars.js')
 }))
 app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname,'resources','views'))
